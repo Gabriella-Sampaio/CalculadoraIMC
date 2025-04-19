08fbffd03e57baa0b44bc706aafb6d8bc40070da
Gabriella_Sampaio <dixxgabriellasam@gmail.com>
2025-04-19 11:44:40 -0300

    if aninhado e TryParse

--- Program.cs
+++ Program.cs
@@ -1,45 +1,83 @@
 ﻿Console.Clear();
 
+
+
+
 Console.WriteLine("Digite seu peso em kg: ");
+double peso;
+string pesoString = Console.ReadLine()!;
+bool pesoValido = Double.TryParse(pesoString, out peso); //TryParse vai TENTAR converter, se tiver letra n vai conseguir e vai dar falso
+
+/*
 double peso = Convert.ToDouble(Console.ReadLine());
+*/
 
 Console.WriteLine("Digite sua altura em m: ");
-string alturastring = Console.ReadLine()!.Replace(".", ",");
-double altura = double.Parse(alturastring); //Converte uma string em double
+double altura;
+string alturastring = Console.ReadLine()!.Replace(".", ","); //da pra alterar o formato do país
+bool alturaValido = double.TryParse(alturastring, out altura);
 
-double resultIMC = peso / (altura * altura);
+/*
+double altura = double.Parse(alturastring); outra opção usando Parse (não recomendado)
+Parse e TryParse converte uma string em variável numérica, no caso double
+bool verifica se o TryParse é verdadeiro
+*/
 
-if (resultIMC <17)
+if (alturaValido && pesoValido) //se forem verdadeiras (! para inverter, se n for verdadeira)
 {
-    Console.WriteLine("Muito a baixo do peso"); 
-}
+    if (peso <= 0 || altura <= 0 || peso > 400 || altura > 3) //inclui números negativos; 
+    // Da pra incluir td no último if cm ||, ms compliquei pra treinar e pra exibir a msg q eu quero nesse caso
+    {
+        Console.WriteLine("Número inválido, tente novamente");
+    }
 
-else if (resultIMC <18.5 )
-{
-    Console.WriteLine("Abaixo do peso");
-}
+    else
+    {
+    double resultIMC = peso / (altura * altura);
 
-else if (resultIMC <25)
-{
-    Console.WriteLine("Peso normal");
-}
+        if (resultIMC <17)
+        {
+            Console.WriteLine("Muito a baixo do peso"); 
+        }
 
-else if (resultIMC <30)
-{
-    Console.WriteLine("Acima do peso");
-}
+        else if (resultIMC <18.5 )
+        {
+            Console.WriteLine("Abaixo do peso");
+        }
 
-else if (resultIMC <35)
-{
-    Console.WriteLine("Obesidade I");
-}
+        else if (resultIMC <25)
+        {
+            Console.WriteLine("Peso normal");
+        }
 
-else if (resultIMC <40)
-{
-    Console.WriteLine("Obesidade II (severa)");
+        else if (resultIMC <30)
+        {
+            Console.WriteLine("Acima do peso");
+        }
+
+        else if (resultIMC <35)
+        {
+            Console.WriteLine("Obesidade I");
+        }
+
+        else if (resultIMC <40)
+        {
+            Console.WriteLine("Obesidade II (severa)");
+        }
+
+        else
+        {
+            Console.WriteLine("Obesidade III (mórbida)");
+        } 
+    }
 }
 
 else
 {
-    Console.WriteLine("Obesidade III (mórbida)");
+    Console.WriteLine("Por favor, digite somente números");
 }
+
+Thread.Sleep(500);
+Console.WriteLine("Obrigada por utilizar nossa calculadora\nBy Bii Sam✨");
+Thread.Sleep(1000);
+Environment.Exit(-1); //termina o programa com um código com erro
